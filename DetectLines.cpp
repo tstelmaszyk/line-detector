15f01dfbdefa8d0a00392a04ba46de73de2987bd
theo <tstelmaszyk>
2024-07-17 21:55:54 +0100

    Deplacement de la classe DetectLine dans un fichier à part

---
+++ DetectLines.cpp
@@ -0,0 +1,104 @@
+#include "DetectLines.h"
+
+DetectLines::DetectLines(const Mat& reference_frame):    width_pixel(reference_frame.size().width),
+                                                height_pixel(reference_frame.size().height)
+    {
+    };
+
+void DetectLines::draw_lines (const Mat& frame_to_compute, Mat& frame_with_lines)
+    {
+        cv::Mat output_gray;
+        grayscal(frame_to_compute,output_gray);
+        //namedWindow( "output gray", cv::WINDOW_KEEPRATIO);
+        //cv::imshow("output gray", output_gray);
+
+        cv::Mat output_gaussian_blur;
+        median_blur(output_gray,output_gaussian_blur);
+        //namedWindow( "Gaussian", cv::WINDOW_KEEPRATIO);
+        //cv::imshow("Gaussian", output_gaussian_blur);
+
+        cv::Mat output_canny ;
+        canny_edge_detection(output_gaussian_blur,output_canny);
+        //namedWindow( "Canny", cv::WINDOW_KEEPRATIO);
+        //cv::imshow("Canny",output_canny);
+
+        frame_with_lines = frame_to_compute ;
+        hough_lines(output_canny,frame_with_lines);
+    }
+
+void DetectLines::grayscal (const Mat& frame_to_compute, Mat& frame_computed)
+    {
+        cv::Mat prov ;
+        bilateralFilter(frame_to_compute,prov,5,250,250);
+        cv::cvtColor(prov,frame_computed,COLOR_RGB2GRAY);
+    }
+
+        
+void DetectLines::gaussian_blur(const Mat& frame_to_compute, Mat& frame_computed)
+    {
+        const cv::Size kernel_size(35,35) ;
+        GaussianBlur(frame_to_compute,frame_computed,kernel_size,0);
+    }
+
+void DetectLines::median_blur(const Mat& frame_to_compute, Mat& frame_computed)
+    {
+        medianBlur(frame_to_compute,frame_computed,13);
+    }
+
+
+void DetectLines::canny_edge_detection(const Mat& frame_to_compute, Mat& frame_computed)
+    {
+        const double low_threshold = 100;
+        const double high_threshold = 200;
+        cv::Canny(frame_to_compute,frame_computed,low_threshold,high_threshold,3,false); 
+    }
+
+
+void DetectLines::hough_lines( const Mat& frame_to_compute,Mat& frame_with_lines_drew)
+    {
+        std::vector<Vec4i> lines;
+        double angle = 0.0 ;
+        const double rho = 1 ;
+        const double theta = CV_PI/180 ;
+        const int threshold = 15 ;
+        const double min_line_height =  height_pixel / 5 ;  
+        const double max_line_gap = 80 ;
+        
+        HoughLinesP(    frame_to_compute, 
+                        lines, 
+                        rho, 
+                        theta, 
+                        threshold, 
+                        min_line_height, 
+                        max_line_gap ); //Output vector of lines. Each line is represented by a 4-element vector x_1, y_1, x_2, y_2), 
+                                        //where x_1,y_1)and x_2, y_2)are the ending points of each detected line segment.
+        
+        std::vector<Vec4i>::iterator iter = lines.begin();
+        while (iter != lines.end())
+            {
+                angle = compute_angle_from_two_points(  Point((*iter)[0], (*iter)[1]),
+                                                        Point((*iter)[2], (*iter)[3]));
+                if (angle > 40)
+                {
+                line(   frame_with_lines_drew, 
+                        Point((*iter)[0], (*iter)[1]),
+                        Point((*iter)[2], (*iter)[3]), 
+                        Scalar(0,0,255), 3, 8 );
+                }
+                else
+                {
+                    //lines.erase(iter); segmentation fault when empty ?
+                }
+                ++iter;
+            }
+    }
+
+double DetectLines::compute_angle_from_two_points (cv::Point point_a, cv::Point point_b) 
+    {
+        double cos_angle = 0.0 ;
+        double angle = 0.0 ;
+        const double rad_to_degree = 180/CV_PI ;
+        cos_angle = (point_b.x-point_a.x) / sqrt( (point_b.x-point_a.x)*(point_b.x-point_a.x) + (point_b.y-point_a.y)*(point_b.y-point_a.y) );
+        angle = acos(cos_angle) * rad_to_degree;
+        return angle ;
+    }
---
+++ DetectLines.h
@@ -0,0 +1,66 @@
+#ifndef _detect_lines_h
+#define _detect_lines_h
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <math.h>
+#include "projectTypes.h"
+
+using namespace cv;
+
+class DetectLines
+    {
+        public:
+            DetectLines(const Mat& reference_frame);
+            void draw_lines (const Mat& frame_to_compute, Mat& frame_with_lines);
+
+        private :
+            const DimensionImage width_pixel ;
+            const DimensionImage height_pixel;
+
+            /*!
+            *  \brief Step 1: Grayscale
+            *  First of all, we want to make the image into a grayscale one; only one color channel. 
+            *  This will help us with the identification of edges and corners.
+            *  \param 
+            */
+            void grayscal (const Mat& frame_to_compute, Mat& frame_computed);
+
+            /*!
+            *  \brief Step 2: Gaussian Blur
+            *  Adding Gaussian noise to an image, it very useful as it smooths the interpolation between the pixels and is a way 
+            *  to super-pass noise and spurious gradients. Higher the kernel, the more blur the outcome image will be.
+            *  https://pyimagesearch.com/2021/04/28/opencv-smoothing-and-blurring/
+            *  \param 
+            */
+            void gaussian_blur(const Mat& frame_to_compute, Mat& frame_computed);
+
+            /*!
+            *  \brief
+            *  \param 
+            */
+            void median_blur(const Mat& frame_to_compute, Mat& frame_computed);
+
+            /*!
+            *  \brief Step 3: Canny Edge Detection
+            *  Canny Edge Detection offers a way to detect the boundaries of an image. This is done through the gradients of the image.
+            *  The latter is nothing more that a function, where the brightness of each pixel corresponds to the strength of the gradient .
+            *  We will find the edges by tracing the pixels that follow the strongest gradients! As in general the gradients show how rapidly 
+            *  a function changes, an intense density change between the pixels will indicate an edge.
+            *  \param 
+            */
+            void canny_edge_detection(const Mat& frame_to_compute, Mat& frame_computed);
+
+            /*!
+            *  \brief
+            *  \param 
+            */
+            void hough_lines( const Mat& frame_to_compute,Mat& frame_with_lines_drew);
+
+            /*!
+            *  \brief Compute angle angle between line and x-axis
+            *  \param Two points in the line
+            */
+            double compute_angle_from_two_points (cv::Point point_a, cv::Point point_b) ;
+    };
+#endif
