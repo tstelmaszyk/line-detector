04e582e7aeb4514dcb6aacd6c4c8f1568e74936e
Théo Stelmaszyk <theo.svk@gmail.com>
2026-06-21 16:36:40 +0200

    refactor(main): lit/écrit l'image via arguments et retire la caméra
    Remplace le chemin d'image codé en dur par des arguments (entrée/sortie)
    et écrit le résultat avec imwrite au lieu d'afficher une fenêtre imshow,
    afin de pouvoir tourner sans écran ni caméra (notamment en conteneur).
    Supprime le code VideoCapture inutilisé par le pipeline.

--- main.cpp
+++ main.cpp
@@ -2,71 +2,42 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-
-#include <libcamera/libcamera.h>
 
 #include "DetectLines.h"
 #include "RegionOfInterest.h"
 #include "VideoCaracteristics.h"
 
 #include <iostream>
-#include <sstream>
-#include <new>
 #include <string>
-#include <sstream>
-
-
-
-#define INPUT_WIDTH 3264
-#define INPUT_HEIGHT 2464
-
-#define DISPLAY_WIDTH 640
-#define DISPLAY_HEIGHT 480
-
-#define CAMERA_FRAMERATE 21/1
-#define FLIP 2
 
 using namespace cv;
 
 
 int main(int argc, char** argv )
 {
-    cv::Mat image;
-    cv::Mat image_camera;
-    cv::Mat image_out ;
+    // Chemins en arguments (pratique en conteneur) ; valeurs par defaut sinon.
+    //   argv[1] = image d'entree, argv[2] = image de sortie.
+    std::string input_path  = (argc > 1) ? argv[1] : "img_piste/img2.jpg";
+    std::string output_path = (argc > 2) ? argv[2] : "output.jpg";
 
-    int height(1080);
-    int width(1920);
-    cv::VideoCapture cap(0);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
-    cap.set(cv::CAP_PROP_FOURCC, 0x21);
-    cap.set(cv::CAP_PROP_FPS, 30);
-    if (!cap.isOpened())
-    {
-        std::cout << "Unable to get video from the camera!" << std::endl;
-        return -1;
-    }
-    std::cout << "Got here!" << std::endl;
-    cap.read(image_camera);
-
-
-
-    image = imread("/home/tsvk/Documents/line-detector/img_piste/img2.jpg",IMREAD_COLOR);
+    cv::Mat image = imread(input_path, IMREAD_COLOR);
     if ( !image.data )
     {
-        printf("No image data \n");
+        std::cout << "Impossible de lire l'image : " << input_path << std::endl;
         return -1;
     }
 
-
+    cv::Mat image_out;
     VideoCaracteristics video_properties (image);
     DetectLines detecteur(video_properties);
-    detecteur.draw_lines(image,image_out);
+    detecteur.draw_lines(image, image_out);
 
-    namedWindow( "Detected Lines", cv::WINDOW_AUTOSIZE);
-    imshow( "Detected Lines", image_out );
-    waitKey(0);
+    // Pas de fenetre GUI dans un conteneur : on ecrit le resultat sur disque.
+    if ( !imwrite(output_path, image_out) )
+    {
+        std::cout << "Impossible d'ecrire l'image de sortie : " << output_path << std::endl;
+        return -1;
+    }
+    std::cout << "Image traitee ecrite dans : " << output_path << std::endl;
     return 0;
 }
